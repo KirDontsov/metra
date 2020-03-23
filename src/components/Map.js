a6fb8fea1b0a2b142ff020331fe6c994b7d4b580
Kir <kir.dontsov@gmail.com>
2020-03-23 22:59:57 +0300

    23.03.2020

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,8 @@
 import React, { Component, Fragment } from "react";
-import {
-  Map as LeafletMap,
-  // TileLayer,
-  Marker,
-  Popup,
-  ZoomControl,
-  GeoJSON
-} from "react-leaflet";
+import { Map as LeafletMap, TileLayer, Marker, Popup, ZoomControl, GeoJSON } from "react-leaflet";
 import RotatedMarker from "./RotatedMarker";
 import L from "leaflet";
 import { connect } from "react-redux";
-import MapboxLayer from "./MapBoxGl";
 import axios from "axios";
 import _ from "lodash";
 import { default as bezierSpline } from "@turf/bezier-spline";
@@ -19,110 +11,100 @@ import carIcon from "../assets/img/car.png";
 import "../scss/Map.scss";
 
 const greenIcon = L.icon({
-  iconUrl: carIcon,
-  shadowUrl: null,
-  shadowSize: [0, 0],
-  shadowAnchor: [0, 0],
-  iconSize: [20, 40], // size of the icon
-  iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-  popupAnchor: [-3, -6]
+	iconUrl: carIcon,
+	shadowUrl: null,
+	shadowSize: [0, 0],
+	shadowAnchor: [0, 0],
+	iconSize: [20, 40], // size of the icon
+	iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+	popupAnchor: [-3, -6]
 });
 
-const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1Ijoia2lyZG9udHNvdiIsImEiOiJjazZjNnZka2EwODJjM29xeDdscHcwYnAyIn0.jR6sL1P44Vjaybu-Ty2hdg";
 
 class MyMap extends Component {
-  request(that) {
-    const { setItems } = that.props;
-    axios.get("http://taxi.tools:8000/cabsformetrasite").then(({ data }) => {
-      setItems(_.values(data.carsList));
-      setTimeout(() => {
-        that.request(that);
-        this.setPath();
-      }, 3000);
-    });
-  }
+	request(that) {
+		const { setItems } = that.props;
+		axios.get("http://taxi.tools:8000/cabsformetrasite").then(({ data }) => {
+			setItems(_.values(data.carsList));
+			setTimeout(() => {
+				that.request(that);
+				this.setPath();
+			}, 3000);
+		});
+	}
 
-  setPath() {
-    const { res } = this.props;
-    const points = res !== "" ? res.OrderCalc.pointsway : null;
-    const line = res !== "" ? helpers.lineString(points) : null;
-    const curved = res !== "" ? bezierSpline(line) : null;
+	setPath() {
+		const { res } = this.props;
+		const points = res !== "" ? res.OrderCalc.pointsway : null;
+		const line = res !== "" ? helpers.lineString(points) : null;
+		const curved = res !== "" ? bezierSpline(line) : null;
 
-    return res !== "" ? (
-      <GeoJSON data={curved} key={Math.random()} style={{ weight: 3 }} />
-    ) : null;
-  }
+		return res !== "" ? <GeoJSON data={curved} key={Math.random()} style={{ weight: 3 }} /> : null;
+	}
 
-  componentDidMount() {
-    this.request(this);
-  }
+	componentDidMount() {
+		this.request(this);
+	}
 
-  render() {
-    const {
-      items,
-      isReady,
-      zoom,
-      latitude,
-      longitude,
-      firstAddress,
-      secondAddress,
-      additionalAddress,
-      didFetched1,
-      didFetched2,
-      didFetched3
-    } = this.props;
-    let position = [latitude, longitude];
+	render() {
+		const {
+			items,
+			isReady,
+			zoom,
+			latitude,
+			longitude,
+			firstAddress,
+			secondAddress,
+			additionalAddress,
+			didFetched1,
+			didFetched2,
+			didFetched3
+		} = this.props;
+		let position = [latitude, longitude];
 
-    const pos1 =
-      firstAddress !== "" && didFetched1 === true
-        ? firstAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-            .split(" ")
-            .reverse()
-            .map(value => parseFloat(value))
-        : null;
+		const pos1 =
+			firstAddress !== "" && didFetched1 === true
+				? firstAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+						.split(" ")
+						.reverse()
+						.map(value => parseFloat(value))
+				: null;
 
-    const pos2 =
-      secondAddress !== "" && didFetched2 === true
-        ? secondAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-            .split(" ")
-            .reverse()
-            .map(value => parseFloat(value))
-        : null;
+		const pos2 =
+			secondAddress !== "" && didFetched2 === true
+				? secondAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+						.split(" ")
+						.reverse()
+						.map(value => parseFloat(value))
+				: null;
 
-    const pos3 =
-      additionalAddress !== "" && didFetched3 === true
-        ? additionalAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-            .split(" ")
-            .reverse()
-            .map(value => parseFloat(value))
-        : null;
+		const pos3 =
+			additionalAddress !== "" && didFetched3 === true
+				? additionalAddress.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+						.split(" ")
+						.reverse()
+						.map(value => parseFloat(value))
+				: null;
 
-    if (firstAddress !== "" && didFetched1 === true) {
-      position = [this.props.latitude, this.props.longitude];
-      this.props.setZoom(17.5);
-    }
+		if (firstAddress !== "" && didFetched1 === true) {
+			position = [this.props.latitude, this.props.longitude];
+			this.props.setZoom(13);
+		}
 
-    return (
-      <Fragment>
-        <LeafletMap
-          center={position}
-          zoom={zoom}
-          zoomControl={false}
-          maxZoom={20}
-          minZoom={4}
-        >
-          {/* <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" /> */}
-          <MapboxLayer
+		return (
+			<Fragment>
+				<LeafletMap center={position} zoom={zoom} zoomControl={false} maxZoom={20} minZoom={4}>
+					<TileLayer url="http://taxi.tools:8000/tilesmass.{s}/{z}/{x}/{y}.png" />
+					{/* <MapboxLayer
             accessToken={MAPBOX_ACCESS_TOKEN}
             style="mapbox://styles/kirdontsov/ck6cbhtnf27vu1inwoo33gdcx"
-          />
+          /> */}
 
-          <ZoomControl position="bottomright" />
+					<ZoomControl position="bottomright" />
 
-          {this.setPath()}
+					{this.setPath()}
 
-          {/* {this.state.haveUsersLocation ? (
+					{/* {this.state.haveUsersLocation ? (
             <Marker position={position}>
               <Popup>
                 <span>Здесь находитесь Вы</span>
@@ -132,81 +114,75 @@ class MyMap extends Component {
             ""
           )} */}
 
-          {pos1 ? (
-            <Marker position={pos1}>
-              <Popup>
-                <span>Заберем Вас здесь</span>
-              </Popup>
-            </Marker>
-          ) : (
-            ""
-          )}
-          {pos2 ? (
-            <Marker position={pos2}>
-              <Popup>
-                <span>Поедем сюда</span>
-              </Popup>
-            </Marker>
-          ) : (
-            ""
-          )}
-          {pos3 ? (
-            <Marker position={pos3}>
-              <Popup>
-                <span>Заедем по пути сюда</span>
-              </Popup>
-            </Marker>
-          ) : (
-            ""
-          )}
+					{pos1 ? (
+						<Marker position={pos1}>
+							<Popup>
+								<span>Заберем Вас здесь</span>
+							</Popup>
+						</Marker>
+					) : (
+						""
+					)}
+					{pos2 ? (
+						<Marker position={pos2}>
+							<Popup>
+								<span>Поедем сюда</span>
+							</Popup>
+						</Marker>
+					) : (
+						""
+					)}
+					{pos3 ? (
+						<Marker position={pos3}>
+							<Popup>
+								<span>Заедем по пути сюда</span>
+							</Popup>
+						</Marker>
+					) : (
+						""
+					)}
 
-          {!isReady
-            ? "Загрузка..."
-            : items.map((item, i) => {
-                let pos = [item.latitude, item.longitude];
-                // console.log(item);
-                return (
-                  <RotatedMarker
-                    key={i}
-                    position={pos}
-                    icon={greenIcon}
-                    rotationAngle={item.course}
-                    rotationOrigin={"center"}
-                  >
-                    <Popup>
-                      <span>{item.CarModel}</span>
-                    </Popup>
-                  </RotatedMarker>
-                );
-              })}
-        </LeafletMap>
-      </Fragment>
-    );
-  }
+					{!isReady
+						? "Загрузка..."
+						: items.map((item, i) => {
+								let pos = [item.latitude, item.longitude];
+								// console.log(item);
+								return (
+									<RotatedMarker key={i} position={pos} icon={greenIcon} rotationAngle={item.course} rotationOrigin={"center"}>
+										<Popup>
+											<span>{item.CarModel}</span>
+										</Popup>
+									</RotatedMarker>
+								);
+						  })}
+				</LeafletMap>
+			</Fragment>
+		);
+	}
 }
 
 const mapState = state => ({
-  items: state.setItems.items,
-  isReady: state.setItems.isReady,
-  firstAddress: state.Quiz.data1,
-  secondAddress: state.Quiz.data2,
-  additionalAddress: state.Quiz.data3,
-  didFetched1: state.Quiz.didFetched1,
-  didFetched2: state.Quiz.didFetched2,
-  didFetched3: state.Quiz.didFetched3,
-  latitude: state.city.latitude,
-  longitude: state.city.longitude,
-  zoom: state.city.zoom,
-  res: state.Quiz.res
+	items: state.setItems.items,
+	isReady: state.setItems.isReady,
+	firstAddress: state.Quiz.data1,
+	secondAddress: state.Quiz.data2,
+	additionalAddress: state.Quiz.data3,
+	didFetched1: state.Quiz.didFetched1,
+	didFetched2: state.Quiz.didFetched2,
+	didFetched3: state.Quiz.didFetched3,
+	latitude: state.city.latitude,
+	longitude: state.city.longitude,
+	zoom: state.city.zoom,
+	res: state.Quiz.res
 });
 
-const mapDispatch = ({
-  setItems: { setItems },
-  city: { setCity, setZoom }
-}) => ({
-  setItems,
-  setCity,
-  setZoom
+const mapDispatch = ({ setItems: { setItems }, city: { setCity, setZoom } }) => ({
+	setItems,
+	setCity,
+	setZoom
 });
 
-export default connect(mapState, mapDispatch)(MyMap);
+export default connect(
+	mapState,
+	mapDispatch
+)(MyMap);
